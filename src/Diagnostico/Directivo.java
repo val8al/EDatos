ed014e00ba4050aaaf7632d8b7b44677d5cdca3e
Valentin <val8al@hotmail.com>
2017-08-28 09:21:28 -0500

    Revert "Práctica 2 listas ligadas"
    This reverts commit ca4527b3f9f977e36dc862fc4b228b247cfa0983.

--- src/Diagnostico/Directivo.java
+++ src/Diagnostico/Directivo.java
@@ -1,9 +1,8 @@
 /*
-Autores:
-Gerardo López Chacón
-Valentín Ochoa López
-
- [*/
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Diagnostico;
 
 public class Directivo extends Planta implements Administrativo{
