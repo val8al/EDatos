c53cc721004d6550db16b5005b57d6eb6315891a
Valentin <val8al@hotmail.com>
2017-10-02 11:08:24 -0500

    busquedas + binarySearch()

--- src/busquedas/Main.java
+++ src/busquedas/Main.java
@@ -11,17 +11,41 @@ package busquedas;
  */
 public class Main {
     public static void main(String[] args) {
+       int[] arreglo={1,2,5,8,33,66,420,421};
        
+       System.out.println(linearSearch(arreglo,420));
+       System.out.println(binarySearch(arreglo,420));
+       System.out.println(binarySearch(arreglo,11));
     }
     public static int linearSearch(int[] list,int target){
-    for(int i=0;i<list.length;i++){
-        if(list[i]==target){
-            return 1;
+        for(int i=0;i<list.length;i++){
+            if(list[i]==target){
+                return 1;
+            }
+            if(list[i]>target){
+                return -1;
+            }
         }
-        if(list[i]>target){
-            return -1;
+    return -1;
+
+    }
+    
+    public static int binarySearch(int [] arr,int target){
+        int min=0;
+        int max=arr.length-1;
+        
+        while(min<=max){
+            int mid=(min+max)/2;
+            if(target>arr[mid]){
+                max=mid-1;
+            }
+            if(target>arr[mid]){
+                min=mid+1;
+            }
+            else{
+                return mid;
+            }
         }
         return -1;
     }
-}
 }
