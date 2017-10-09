3cb75d3e6d33fbd352aedec36e1baba8b61db8f6
Valentin <val8al@hotmail.com>
2017-10-09 09:07:17 -0500

    algoritmos ordenamiento: insertionSort, selectionSort

---
+++ src/Ordenamientos/Main.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ordenamientos;
+import java.util.Arrays;
+
+/**
+ *
+ * @author val8a_000
+ */
+public class Main {
+    public static void main(String[] args){
+        int[] arreglo={2,7,8,9,420,15,11,14};
+        //System.out.println(Arrays.toString(shiftRight(arreglo,2,5)));
+        selectionSort(arreglo);
+        insertionSort(arreglo);
+        
+    }
+    public static int[] shiftRight(int[] arr, int start, int end){
+       int[] newArr=new int[arr.length];
+       for(int i=0;i<start;i++){
+           newArr[i]=arr[i];
+       }
+       for(int i=start;i<end;i++){
+           newArr[i+1]=arr[i];
+       }
+       for(int i=end+1;i<arr.length;i++){
+           newArr[i]=arr[i];
+       }
+       return newArr;
+    }
+    public static void insertionSort(int[] arr){
+        int tommy;
+        for(int i=1;i<arr.length;i++){
+            for(int j=0;j<i;j++){
+                if(arr[i]<=arr[j]){
+                    tommy=arr[i];
+                    arr=shiftRight(arr,j,i);
+                    arr[j]=tommy;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void selectionSort(int[] arr){
+       
+        
+        for(int i=0;i<arr.length;i++){
+          int current=arr[i];
+          for(int j=i;j<arr.length;j++){
+              if(arr[j]<=arr[i]){
+                  arr[i]=arr[j];
+                  arr[j]=current;
+                  current=arr[i];
+              }
+          }
+        }
+        System.out.println(Arrays.toString(arr));
+        
+        
+    }
+    
+    
+    
+    
+}
